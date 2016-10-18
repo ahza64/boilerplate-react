ea8eccf83daddf4df7ee46545b01e2fc43f65eec
ahza64 <ahza64@yahoo.com>
2016-10-17 23:54:28 -0700

    nesting components

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,26 @@
 
+var GreeterMessage = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Some h1</h1>
+        <p>Some paragraph</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter = React.createClass({
 
   getDefaultProps: function () {
@@ -37,10 +59,15 @@ var Greeter = React.createClass({
         <h1>Hello {name}</h1>
         <p>{message}</p>
 
+        <GreeterMessage/>
+
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button>Set Name</button>
         </form>
+
+        <GreeterForm/>
+
       </div>
     );
   }
