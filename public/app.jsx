eb9cdd4be3b5e3c297a687b8d4beb91a70248e62
ahza64 <ahza64@yahoo.com>
2016-10-17 23:25:10 -0700

    passing a second prop

--- public/app.jsx
+++ public/app.jsx
@@ -3,25 +3,28 @@ var Greeter = React.createClass({
 
   getDefaultProps: function () {
     return {
-      name: "React's default prop for name"
+      name: "React's default prop for name",
+      message: "React's default prop for message"
     };
   },
 
   render: function () {
     var name = this.props.name;
+    var message = this.props.message;
 
     return (
       <div>
         <h1>Hello {name}</h1>
-        <p>This is from a component</p>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
 var firstName = 'Justin'
+var message;
 
 ReactDOM.render(
-  <Greeter name={firstName}/>,
+  <Greeter name={firstName} message={message}/>,
   document.getElementById('app')
 );
